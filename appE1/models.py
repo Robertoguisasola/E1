131324424d8b26b54f50767ddc9f2c7135760df7
Roberto <Roberto@portatil>
2019-11-25 17:15:13 +0100

    Meter css, no saca el estilo adecuado

--- appE1/models.py
+++ appE1/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from datetime import datetime
+
 
 # Create your models here.
+class Articulo(models.Model):
+    titular = models.CharField(max_length=100, default='')
+    entradilla = models.TextField()
+    cuerpo = models.TextField()
+    fecha = models.DateTimeField(default=datetime.now, blank=True)
+
+    @property
+    def __str__(self):
+        return self.titular
