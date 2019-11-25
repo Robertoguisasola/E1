b6b83f405582427e887c7c32093c1ae480409df9
Roberto <Roberto@TorreBilbao>
2019-11-25 17:43:28 +0100

    Saca el css bien, no se xq fallaba y ya están los models creados

--- appE1/models.py
+++ appE1/models.py
@@ -3,12 +3,19 @@ from datetime import datetime
 
 
 # Create your models here.
+class Autor(models.Model):
+    nombre = models.CharField(max_length=99, default='')
+    apellido = models.CharField(max_length=99, default='')
+
+    def __str__(self):
+        return self.nombre
+
+
 class Articulo(models.Model):
     titular = models.CharField(max_length=100, default='')
     entradilla = models.TextField()
-    cuerpo = models.TextField()
+    noticia = models.TextField()
     fecha = models.DateTimeField(default=datetime.now, blank=True)
 
-    @property
     def __str__(self):
         return self.titular
