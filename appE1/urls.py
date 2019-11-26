4329c48c2b533babba7d9d00f99fd48813f729dd
Roberto <Roberto@TorreBilbao>
2019-11-26 01:34:11 +0100

    Index creada, completarla

--- appE1/urls.py
+++ appE1/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('articulos/', views.articulos, name='articulos'),
     path('articulo/<int:articulo_id>/', views.detail, name='articulo')
 ]
--- appE1/views.py
+++ appE1/views.py
@@ -3,14 +3,19 @@ from django.shortcuts import render, get_list_or_404, get_object_or_404
 from appE1.models import Articulo
 
 
-# Devuelve un listado de post
 def index(request):
+    return render(request, 'index.html')
+
+
+# Devuelve un listado de post
+def articulos(request):
     articulos = get_list_or_404(Articulo.objects.order_by('titular'))
     context = {'listaArticulos': articulos}
-    return render(request, 'index.html', context)
+    return render(request, 'articulos.html', context)
 
 
+# Devuelve los datos de un post
 def detail(request, articulo_id):
-    departamento = get_object_or_404(Articulo, pk=articulo_id)
-    context = {'departamento': departamento}
+    articulo = get_object_or_404(Articulo, pk=articulo_id)
+    context = {'articulo': articulo}
     return render(request, 'detail.html', context)
