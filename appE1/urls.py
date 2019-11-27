e62f27d7950843dbff264b96ab883eeea7f50f66
Roberto <Roberto@portatil>
2019-11-27 12:06:01 +0100

    Publica los post con sus diferentes enlaces y saca cada post

--- appE1/urls.py
+++ appE1/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('home/', views.index, name='index'),
     path('articulos/', views.articulos, name='articulos'),
-    path('articulo/<int:articulo_id>/', views.detail, name='articulo')
+    path('articulo/<int:articulo_id>/', views.articulo, name='articulo')
 ]
--- appE1/views.py
+++ appE1/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-
 from appE1.models import Articulo
 
 
@@ -7,15 +6,15 @@ def index(request):
     return render(request, 'index.html')
 
 
-# Devuelve un listado de post
+# Devuelve un listado de artículos
 def articulos(request):
     articulos = get_list_or_404(Articulo.objects.order_by('titular'))
     context = {'listaArticulos': articulos}
     return render(request, 'articulos.html', context)
 
 
-# Devuelve los datos de un post
-def detail(request, articulo_id):
+# Devuelve los detalles de un artículo
+def articulo(request, articulo_id):
     articulo = get_object_or_404(Articulo, pk=articulo_id)
     context = {'articulo': articulo}
-    return render(request, 'detail.html', context)
+    return render(request, 'articulo.html', context)
