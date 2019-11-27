754177506c16eefabe13a66fb79e2589e6765802
Roberto <Roberto@portatil>
2019-11-27 12:24:10 +0100

    Saca el listado de autores, sin referenciarlos a sus artículos

--- appE1/urls.py
+++ appE1/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns = [
     path('home/', views.index, name='index'),
     path('articulos/', views.articulos, name='articulos'),
-    path('articulo/<int:articulo_id>/', views.articulo, name='articulo')
+    path('articulo/<int:articulo_id>/', views.articulo, name='articulo'),
+    path('autores/', views.autores, name='autores'),
+    path('autor/<int:autor_id>/', views.autor, name='autor')
+
 ]
--- appE1/views.py
+++ appE1/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_list_or_404, get_object_or_404
-from appE1.models import Articulo
+from appE1.models import Articulo, Autor
 
 
 def index(request):
@@ -18,3 +18,16 @@ def articulo(request, articulo_id):
     articulo = get_object_or_404(Articulo, pk=articulo_id)
     context = {'articulo': articulo}
     return render(request, 'articulo.html', context)
+
+
+def autores(request):
+    autores = get_list_or_404(Autor.objects.order_by('nombre'))
+    context = {'listaAutores': autores}
+    return render(request, 'autores.html', context)
+
+
+def autor(request, autor_id):
+    autor = get_object_or_404(Autor, pk=autor_id)
+    autor = {'autor': autor}
+    return render(request, 'autor.html', context)
+
